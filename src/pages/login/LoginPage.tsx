466481eaf550e112d8978a61d089ae86a719ff41
mohammedkhattab2 <dimakhattab2017@gmail.com>
2025-11-24 19:28:29 +0200

    change login background

--- src/pages/login/LoginPage.tsx
+++ src/pages/login/LoginPage.tsx
@@ -55,7 +55,7 @@ const LoginPage = () => {
 
       {/* Noise texture overlay */}
       <div
-        className="fixed inset-0 opacity-[0.015] -z-10"
+        className="fixed inset-0 opacity-10 -z-10"
         style={{
           backgroundImage: `url("data:image/svg+xml,%3Csvg viewBox='0 0 400 400' xmlns='http://www.w3.org/2000/svg'%3E%3Cfilter id='noiseFilter'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='0.9' numOctaves='4' /%3E%3C/filter%3E%3Crect width='100%25' height='100%25' filter='url(%23noiseFilter)' /%3E%3C/svg%3E")`,
         }}
@@ -74,12 +74,12 @@ const LoginPage = () => {
       </div>
 
       {/* Grid pattern overlay */}
-      <div
+      {/* <div
         className="fixed inset-0 -z-10 opacity-[0.03]"
         style={{
           backgroundImage: `url("data:image/svg+xml,%3Csvg width='60' height='60' viewBox='0 0 60 60' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='none' fill-rule='evenodd'%3E%3Cg fill='%23ffffff' fill-opacity='1'%3E%3Cpath d='M36 34v-4h-2v4h-4v2h4v4h2v-4h4v-2h-4zm0-30V0h-2v4h-4v2h4v4h2V6h4V4h-4zM6 34v-4H4v4H0v2h4v4h2v-4h4v-2H6zM6 4V0H4v4H0v2h4v4h2V6h4V4H6z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E")`,
         }}
-      />
+      /> */}
 
       {/* Desktop: Brand panel (left side) */}
       <div className="hidden lg:flex lg:flex-1 lg:flex-col lg:justify-center lg:px-16 lg:py-24">
